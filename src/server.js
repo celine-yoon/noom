2367ae73c02a8feac2b7a6631f309aef8af8fabb
Celine Yoon <celine.sy.yoon@gmail.com>
2023-11-09 01:58:14 +0900

    Add: WS

--- src/server.js
+++ src/server.js
@@ -1,10 +1,11 @@
 import express from "express";
 
 const app = express();
-app.set("view engine", "pug");
+app.set("view engine", "pug"); //sets pug as the view engine
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
-app.get("/", (req, res) => res.render("home"));
+app.get("/", (req, res) => res.render("home")); // rendering "home.pug"
+app.get("/*", (req, res) => res.redirect("/")); // redirect to "home" if a user types in any other urls
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 
